6c57098aa6baab11841e871a97510ac75be2f14a
shaloo <shaloobokolia2001@gmail.com>
2025-01-02 22:38:22 +0100

    first commit byshaloo2jan

--- testFirstTest.py
+++ testFirstTest.py
@@ -2,11 +2,13 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 # rajesh
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+def test_run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
+    page.set_default_timeout(15000)
     page.goto("https://practicetestautomation.com/")
+    #page.wait_for_load_state("networkidle")
     page.get_by_role("link", name="Practice", exact=True).click()
     page.get_by_role("link", name="Test Login Page").click()
     page.get_by_label("Username").click()
@@ -15,11 +17,13 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Password").fill("Password123")
     page.get_by_role("button", name="Submit").click()
     page.get_by_role("link", name="Log out").click()
+    #page.pause()
+    print("shaloo")
+    page.set_default_timeout(15000)
 
     # ---------------------
     context.close()
     browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+
